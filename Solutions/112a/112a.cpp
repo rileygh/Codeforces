68ad922c72403bd6ff4c5a4284782f5099f74b55
Jade <jadedevofficial@gmail.com>
2022-12-28 08:26:39 +0000

    init

--- Solutions/112a/112a.cpp
+++ Solutions/112a/112a.cpp
@@ -6,7 +6,7 @@ int main()
 {
     std::string s1{},s2{};
     std::cin>>s1>>s2;
-    for (int i=0;i<s1.length();i++) 
+    for (int i{0};i<s1.length();i++) 
     {
         char c1=std::tolower(s1[i]);
         char c2=std::tolower(s2[i]);
