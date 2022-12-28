249b548b12527d909a90c4c57d649de52748f566
Jade <jadedevofficial@gmail.com>
2022-12-28 03:53:06 +0000

    upd brace assignment

--- Solutions/158a/158a.cpp
+++ Solutions/158a/158a.cpp
@@ -6,12 +6,12 @@ int main()
     int n{},k{};
     std::cin>>n>>k;
     int participants[n]{};
-    for (int i=0;i<n;i++) 
+    for (int i{0};i<n;i++) 
     {
         std::cin>>participants[i];
     }
-    int kth=participants[k-1];
-    int ans=std::count_if(&participants[0],&participants[n],[&](int x){return x > 0 && x >= kth;});
+    int kth{participants[k-1]};
+    int ans{std::count_if(&participants[0],&participants[n],[&](int x){return x > 0 && x >= kth;})};
     std::cout<<ans;
     return 0;
 }
