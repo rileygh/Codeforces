6d4a39370dc3bedd36fc8a1f898aa225574d0b5f
Jade <jadedevofficial@gmail.com>
2022-12-28 09:26:13 +0000

    optimise

--- Solutions/263a/263a.cpp
+++ Solutions/263a/263a.cpp
@@ -1,45 +1,19 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
-#include <vector>
 
 int main()
 {
-    int moves{0};
-    std::vector<std::vector<int>> matrix;
+    int el{0};
     for (int i{0};i<5;i++)
     {
-        std::vector<int> r;
-        for (int j{0}; j < 5; j++)
+        for (int j{0};j<5;j++)
         {
-            int x;
-            std::cin>>x;
-            r.push_back(x);
-        }
-        for (int j:r)
-        {
-            if (j==1&&std::distance(r.begin(),std::find(r.begin(),r.end(),j))!=2)
+            std::cin>>el;
+            if (el==1)
             {
-                int moves_needed{std::abs(2-std::distance(r.begin(),std::find(r.begin(),r.end(),j)))};
-                moves+=moves_needed;
-                r[std::distance(r.begin(),std::find(r.begin(),r.end(),j))]=0;
-                r[2]=1;
-            }
-        }
-        matrix.push_back(r);
-    }
-
-    if (matrix[2][2]!=1)
-    {
-        for (const auto& i:matrix)
-        {
-            if (i[2]==1)
-            {
-                int moves_needed{std::abs(2-std::distance(matrix.begin(),std::find(matrix.begin(),matrix.end(),i)))};
-                moves+=moves_needed;
+                std::cout<<abs(i-2)+abs(j-2)<<std::endl;
             }
         }
     }
-    std::cout<<moves<<std::endl;
     return 0;
 }
