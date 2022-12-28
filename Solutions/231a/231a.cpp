b6bcafb2f53b56c66458485c4abf67ffe0dcee47
Jade <jadedevofficial@gmail.com>
2022-12-28 08:21:54 +0000

    init

---
+++ Solutions/231a/231a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int n{};
+    std::cin>>n;
+    int count{};
+    for (int i=0;i<n;i++) 
+    {
+        int a{}, b{}, c{};
+        std::cin>>a>>b>>c;
+        if (a+b+c>=2)
+        {
+            count++;
+        }
+    }
+    std::cout<<count;
+    return 0;
+}
