8c5cca2366b4b51dde59377b471bf9335d95126d
Jade <jadedevofficial@gmail.com>
2022-12-29 17:36:19 +0000

    format

--- Solutions/339a/main.cpp
+++ Solutions/339a/main.cpp
@@ -11,12 +11,13 @@ int main() {
     std::vector<int> n;
     std::regex nums("\\d+");
     std::for_each(std::sregex_iterator(str.begin(),str.end(),nums),
-                  std::sregex_iterator(),[&n](const std::smatch& match){n.push_back(std::stoi(match.str()));});
+                  std::sregex_iterator(),
+                  [&n](const std::smatch& match){n.push_back(std::stoi(match.str()));});
 
     std::sort(n.begin(),n.end());
 
-    std::string sorted{std::accumulate(n.begin(),n.end(),std::string{},[](const std::string& a,int b) {
-                                       return a+(a.empty()?"":"+")+std::to_string(b);})};
+    std::string sorted{std::accumulate(n.begin(),n.end(),std::string{},[](const std::string& a,int b)
+                                       {return a+(a.empty()?"":"+")+std::to_string(b);})};
 
     std::cout<<sorted;
 
