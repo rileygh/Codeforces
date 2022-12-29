a42d966737c119c346cf9180f9abe28e33721fc4
Jade <jadedevofficial@gmail.com>
2022-12-29 17:34:59 +0000

    init

---
+++ Solutions/339a/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <regex>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+
+int main() {
+    std::string str;
+    std::cin>>str;
+
+    std::vector<int> n;
+    std::regex nums("\\d+");
+    std::for_each(std::sregex_iterator(str.begin(),str.end(),nums),
+                  std::sregex_iterator(),[&n](const std::smatch& match){n.push_back(std::stoi(match.str()));});
+
+    std::sort(n.begin(),n.end());
+
+    std::string sorted{std::accumulate(n.begin(),n.end(),std::string{},[](const std::string& a,int b) {
+                                       return a+(a.empty()?"":"+")+std::to_string(b);})};
+
+    std::cout<<sorted;
+
+    return 0;
+}
