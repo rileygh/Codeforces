0db52fd772b05fb6ab08494f833b85fe91abb4f9
Jade <jadedevofficial@gmail.com>
2023-01-02 15:19:46 +0000

    init

---
+++ Solutions/59a/59a.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    int upper{0};
+    int lower{0};
+    for (char c:s) {
+        if (std::isupper(c)) 
+        {
+            upper++;
+        }
+        else 
+        {
+            lower++;
+        }
+    }
+
+    for (int i{0};i<s.length();i++) 
+    {
+        if (upper>lower)
+        {
+            s[i]=std::toupper(s[i]);
+        }
+        else
+        {
+            s[i]=std::tolower(s[i]);
+        }
+    }
+
+    std::cout<<s;
+
+    return 0;
+}
