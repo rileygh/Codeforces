48ab7d8b289d500f714ae9da5203618d90e06dd6
Jade <jadedevofficial@gmail.com>
2023-01-04 18:22:59 +0000

    init

---
+++ Solutions/977a/977a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() 
+{
+    int n{},k{};
+    std::cin>>n>>k;
+    for (int i{0};i<k;i++)
+    {
+        if (n%10==0&&n>=10)
+        {
+            n/=10;
+        }
+        else 
+        {
+            n--;
+        }
+    }
+    std::cout<<n;
+    return 0;
+}
