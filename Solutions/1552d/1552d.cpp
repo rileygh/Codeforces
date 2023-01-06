2d0183752a5eaf7955c0332dfa2a4ae144243510
Jade <jadedevofficial@gmail.com>
2023-01-06 13:02:24 +0000

    fmt

--- Solutions/1552d/1552d.cpp
+++ Solutions/1552d/1552d.cpp
@@ -4,7 +4,7 @@ int t,n,a[10005];
 int main()
 {
     std::cin>>t;
-    while(t--)
+    while (t--)
     {
         std::cin>>n;
         for (int i{1};i<=n;i++)    
@@ -12,33 +12,33 @@ int main()
             std::cin>>a[i];
         }
         int mask{1},has_zero_sum{-1};
-        for(int i{1};i<=n;i++)    
+        for (int i{1};i<=n;i++)    
         {
             mask*=3;
         }
-        for(int i{1};i<mask;i++)
+        for (int i{1};i<mask;i++)
         {
             int ms{i},sum{0};
-            for(int k{1};k<=n;k++)
+            for (int k{1};k<=n;k++)
             {
                 int flag{ms%3};ms/=3;
-                if(flag==1)    
+                if (flag==1)    
                 {
                     sum+=a[k]*1;
                 }
-                if(flag==2)    
+                if (flag==2)    
                 {
                     sum+=a[k]*-1;
                 }
             }
-            if(sum==0)
+            if (sum==0)
             {
                 has_zero_sum=1;
                 std::cout<<"YES"<<std::endl;
                 break;
             }
         }
-        if(has_zero_sum==-1)    
+        if (has_zero_sum==-1)    
         {
             std::cout<<"NO"<<std::endl;
         }
